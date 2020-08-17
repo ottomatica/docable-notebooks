219f624ac788b93d94f50c052fa99a3bf59fd014
Chris Parnin <chris.parnin@gmail.com>
2020-08-17 10:15:07 -0400

    fix: use correct assignment operator.

--- public/js/notebook.js
+++ public/js/notebook.js
@@ -39,7 +39,7 @@ function run(endPoint, body)
         for (const result of results) {
             // selecting cells using index to adding results
             let block = $('[data-docable="true"]').eq(result.cellindex);
-            if (block.data('type') == 'file' && result.result.status) result.result.stdout == 'Created file successfully.';
+            if (block.data('type') == 'file' && result.result.status) result.result.stdout = 'Created file successfully.';
             let selector = block.parent();
 
             setResults(selector, result.result);
