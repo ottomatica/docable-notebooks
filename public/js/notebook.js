4ddce39809b8e67346bc6d57b2cd80fe72abf4f5
Chris Parnin <chris.parnin@gmail.com>
2020-08-17 12:32:02 -0400

    refactor: introduce HttpModule pattern.

--- public/js/notebook.js
+++ public/js/notebook.js
@@ -27,21 +27,15 @@ function run(endPoint, body, stepIndex)
     submitButtonSpinToggle();
     resetResults(stepIndex);
 
-    fetch(endPoint, {
-        method: 'POST',
-        mode: 'cors',
-        body: body,
-        headers: { "content-type": "application/json; charset=UTF-8" },
-    })
-    .then(response => response.text())
-    .then(data => {
+    executeCells(endPoint, body).then(function(data) {
+
         const results = JSON.parse(data);
         for (const result of results) {
             // selecting cells using index to adding results
             let block = $('[data-docable="true"]').eq(result.cellindex);
             if (block.data('type') == 'file' && result.result.status) result.result.stdout = 'Created file successfully.';
             let selector = block.parent();
-
+    
             setResults(selector, result.result);
         }
 
@@ -58,71 +52,41 @@ $('main').on('click', '.play-btn', function () {
 
 });
 
+const EditForm = ({stepIndex}) =>
+`<div id="update-cell-form">
+    <div class="form-group">
+        <label for="docable-edit-area-${stepIndex}">Edit Cell</label>
+        <textarea rows="5" name="text" class="form-control" id="docable-edit-area-${stepIndex}"></textarea>
+    </div>
+    <button id="btn-cancel-cell" type="submit" class="btn btn-secondary">Cancel</button>
+    <button id="btn-update-cell" class="btn btn-primary">Update</button>
+</div>
+`;
+
 // We use the parent, with child selector because if cells are dynamically updated, then they will not be registered.
 $('main').on('click', '.btn-more', function () {
-//$('.btn-more').on('click', function () {
     let stepIndex = $('pre[data-docable="true"]').index($(this).siblings('pre[data-docable="true"]'));
-    console.log(stepIndex);
-
     let cell = $('[data-docable="true"]').eq(stepIndex);
 
-    let parent = $(this).parent();
+    let cell_overlay = $(this).parent();
+    $(cell_overlay).hide();
 
-    $(parent).hide();
-
-    let form = 
-    `<div id="update-cell-form">
-        <div class="form-group">
-            <label for="docable-edit-area-${stepIndex}">Edit Cell</label>
-            <textarea rows="5" name="text" class="form-control" id="docable-edit-area-${stepIndex}"></textarea>
-        </div>
-        <button id="btn-cancel-cell" type="submit" class="btn btn-secondary">Cancel</button>
-        <button id="btn-update-cell" class="btn btn-primary">Update</button>
-    </div>
-    `;
-
-    parent.parent().append(form);
+    cell_overlay.parent().append( EditForm({stepIndex}) );
 
     $('#btn-cancel-cell').on('click', function () {
-
         $("#update-cell-form").remove();
-        $(parent).show();
-
+        $(cell_overlay).show();
     });
 
     $('#btn-update-cell').on('click', function () {
-
-        fetch('/editCell', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify({ text: $(`textarea[id="docable-edit-area-${stepIndex}"`).val() }),
-            headers: { "content-type": "application/json; charset=UTF-8" },
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        })
-        .then(response => response.text())
+        editCell($(`textarea[id="docable-edit-area-${stepIndex}"`).val())
         .then(data => {
-
-
             let cell = $('[data-docable="true"]').eq(stepIndex);
-
             cell.parent().parent().replaceWith( data );
-
         });
-
     });
 
-    fetch('/viewCell', {
-        method: 'POST',
-        mode: 'cors',
-        body: JSON.stringify({ text: $(cell)[0].outerHTML, stepIndex: stepIndex }),
-        headers: { "content-type": "application/json; charset=UTF-8" },
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    })
-    .then(response => response.json())
+    viewCell($(cell)[0].outerHTML, stepIndex)
     .then(data => {
         console.log(data);
         $(`#docable-edit-area-${stepIndex}`).val(data.cell);
