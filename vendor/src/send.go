43c95523d9d93c4eedd02925a051752c108f99f5
Chris Parnin <chris.parnin@gmail.com>
2020-09-11 07:53:43 -0400

    docs: update documentation for send program.

--- vendor/src/send.go
+++ vendor/src/send.go
@@ -13,11 +13,26 @@ var (
     SendMessage	= user32DLL.NewProc("SendMessageW")
 )
 
+/*
+https://docs.microsoft.com/en-us/windows/win32/procthread/environment-variables
+
+> Calling SetEnvironmentVariable has no effect on the system environment variables. 
+To programmatically add or modify system environment variables, add them to the 
+HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment registry key, 
+then broadcast a WM_SETTINGCHANGE message with lParam set to the string "Environment". 
+This allows applications, such as the shell, to pick up your updates.
+*/
+
 func main() {
 
 	text, _ := windows.UTF16PtrFromString("Environment");
 	fmt.Println("[+] Sending refresh message...")
 	// fmt.Println(err);
+	// #define HWND_BROADCAST ((HWND)0xffff)
+	// #define WM_SETTINGCHANGE                  0x001A
+	// https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-settingchange
+	// Wparam 0
+	// Lparam "Environment"
 	SendMessage.Call(0xffff, 26, 0, uintptr(unsafe.Pointer(text)) );
 	// var buf [256]byte
 	// SendMessageTimeout.Call(0xffff, 26, 0, uintptr(unsafe.Pointer(text)), 0, 5000, uintptr(unsafe.Pointer(&buf[0])));
